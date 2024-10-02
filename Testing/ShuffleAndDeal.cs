ca3c40b5c1b79931fe8e19682071f0f9e73f7d36
Adam O'Neil <adamosoftware@gmail.com>
2024-10-02 09:49:17 -0400

    cleanup

--- Testing/ShuffleAndDeal.cs
+++ Testing/ShuffleAndDeal.cs
@@ -8,53 +8,53 @@ namespace Testing;
 [TestClass]
 public class ShuffleAndDeal
 {
-    [TestMethod]
-    public void HeartsDeal()
-    {
-        var hearts = new HeartsGameFactory();
-        var game = hearts.Start(false, [ "Adam", "Andy", "Dad", "Becky" ]);
+	[TestMethod]
+	public void HeartsDeal()
+	{
+		var hearts = new HeartsGameFactory();
+		var game = hearts.Start(false, [ "Adam", "Andy", "Dad", "Becky" ]);
 
-        foreach (var player in game.Players)
-        {
-            Debug.Print(player.Name);
-            int index = 0;
-            foreach (var card in player.Hand)
-            {
-                index++;
-                Debug.Print("- " + card.ToString() + $" ({index})");
-            }
-        }
+		foreach (var player in game.Players)
+		{
+			Debug.Print(player.Name);
+			int index = 0;
+			foreach (var card in player.Hand)
+			{
+				index++;
+				Debug.Print("- " + card.ToString() + $" ({index})");
+			}
+		}
 
-        Debug.Print($"Current player: {game.CurrentPlayer?.Name ?? "<unknown>"}");
-        Debug.Print($"Leading suit: {game.LeadingSuit.Name}");
+		Debug.Print($"Current player: {game.CurrentPlayer?.Name ?? "<unknown>"}");
+		Debug.Print($"Leading suit: {game.LeadingSuit.Name}");
 
-        Assert.IsTrue(!game.DrawPile.Any());
-    }
+		Assert.IsTrue(!game.DrawPile.Any());
+	}
 
-    /// <summary>
-    /// simulate play so we can see what state is generated
-    /// </summary>
-    [TestMethod]
-    public void HeartsAutoPlay()
-    {
+	/// <summary>
+	/// simulate play so we can see what state is generated
+	/// </summary>
+	[TestMethod]
+	public void HeartsAutoPlay()
+	{
 		var hearts = new HeartsGameFactory();
 		var game = hearts.Start(false, ["Adam", "Andy", "Dad", "Becky"]);
 
-        while (!game.IsFinished) game.AutoPlay();
+		while (!game.IsFinished) game.AutoPlay();
 
-        var json = JsonSerializer.Serialize(game, new JsonSerializerOptions() { WriteIndented = true });        
-        Debug.Print(json);
+		var json = JsonSerializer.Serialize(game, new JsonSerializerOptions() { WriteIndented = true });        
+		Debug.Print(json);
 
-        // for now, I'm just manually inspecting the json output
+		// for now, I'm just manually inspecting the json output
 	}
 
-    [TestMethod]
-    public void SuitEquality()
-    {
-        // I had some trouble with suit equality
-        Assert.AreEqual(ClassicSuits.Clubs, new PlayingCard(2, ClassicSuits.Clubs).Suit);
-        
-        // remember to use the Equals method, not ==
-        //Assert.IsTrue(Suits.Clubs == new Suit("Clubs", 1));
-    }
+	[TestMethod]
+	public void SuitEquality()
+	{
+		// I had some trouble with suit equality
+		Assert.AreEqual(ClassicSuits.Clubs, new PlayingCard(2, ClassicSuits.Clubs).Suit);
+		
+		// remember to use the Equals method, not ==
+		//Assert.IsTrue(Suits.Clubs == new Suit("Clubs", 1));
+	}
 }
