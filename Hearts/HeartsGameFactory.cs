59fdf86d6920e82bb3f8b066d16bf41cb8524a4e
Adam O'Neil <adamosoftware@gmail.com>
2024-10-05 07:04:05 -0400

    cleanup

--- Hearts/HeartsGameFactory.cs
+++ Hearts/HeartsGameFactory.cs
@@ -4,36 +4,36 @@ namespace Games.Hearts;
 
 public class HeartsGameFactory : GameFactory<HeartsGameState, PlayingCard>
 {
-    public override uint MinPlayers => 4;
+	public override uint MinPlayers => 4;
 
-    public override uint MaxPlayers => 4;
+	public override uint MaxPlayers => 4;
 
-    protected override uint CardsPerHand => 12;
+	protected override uint CardsPerHand => 12;
 
 	public override IEnumerable<PlayingCard> Deck => PlayingCard.ClassicDeck;
 
-    public override string Name => "Hearts (4p)";
+	public override string Name => "Hearts (4p)";
 
-    protected override HeartsGameState CreateGameState(
-        bool devMode, 
-        HashSet<Player<PlayingCard>> players, 
-        Dictionary<int, Player<PlayingCard>> byIndex, 
-        Dictionary<string, Player<PlayingCard>> byName,
-        Queue<PlayingCard> drawPile)
-    {       
+	protected override HeartsGameState CreateGameState(
+		bool devMode, 
+		HashSet<Player<PlayingCard>> players, 
+		Dictionary<int, Player<PlayingCard>> byIndex, 
+		Dictionary<string, Player<PlayingCard>> byName,
+		Queue<PlayingCard> drawPile)
+	{       
 		var startPlayer = players.Single(p => p.Hand.Contains(new PlayingCard(2, ClassicSuits.Clubs)));
 
 		HeartsGameState result = new()
-        {
-            IsDevMode = devMode,
-            Players = players,
-            PlayersByIndex = byIndex,
-            PlayersByName = byName,            
-            CurrentPlayer = startPlayer
-        };
+		{
+			IsDevMode = devMode,
+			Players = players,
+			PlayersByIndex = byIndex,
+			PlayersByName = byName,            
+			CurrentPlayer = startPlayer
+		};
 
-        result.PlayCard(new(2, ClassicSuits.Clubs));
+		result.PlayCard(new(2, ClassicSuits.Clubs));
 
-        return result;
-    }
+		return result;
+	}
 }
