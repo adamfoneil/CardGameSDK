ace492a866539f005cc78ae9274046b232749d7b
Adam O'Neil <adamosoftware@gmail.com>
2024-10-08 06:45:33 -0400

    use migrations endpoint

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -48,12 +48,10 @@ builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSe
 
 var app = builder.Build();
 
+app.UseMigrationsEndPoint();
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-	app.UseMigrationsEndPoint();
-}
-else
+if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Error", createScopeForErrors: true);
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
