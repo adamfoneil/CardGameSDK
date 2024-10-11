8212cb6416d44b4ebf41fbfee45869311bbf1fd9
Adam O'Neil <adamosoftware@gmail.com>
2024-10-11 13:56:35 -0400

    not working either

--- BlazorApp/Components/Ready/StateContainer.cs
+++ BlazorApp/Components/Ready/StateContainer.cs
@@ -4,9 +4,9 @@ namespace BlazorApp.Components.Ready;
 
 public class StateContainer
 {
-	public event Func<SupportedGames, Task>? OnPlayersUpdatedAsync;
+	public event Action<SupportedGames>? OnPlayersUpdated;
 	public event Action<SupportedGames, string>? OnGameStarted;
 
-	public void NotifyPlayersUpdated(SupportedGames game) => OnPlayersUpdatedAsync?.Invoke(game);
+	public void NotifyPlayersUpdated(SupportedGames game) => OnPlayersUpdated?.Invoke(game);
 	public void NotifyGameStarted(SupportedGames game, string url) => OnGameStarted?.Invoke(game, url);
 }
