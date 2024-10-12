9aa91e5d07745f423c6c71d7b2ba085ab45955e0
Adam O'Neil <adamosoftware@gmail.com>
2024-10-12 11:39:38 -0400

    consolidated some event stuff

--- BlazorApp/EventBackgroundService.cs
+++ BlazorApp/EventBackgroundService.cs
@@ -1,6 +1,7 @@
 ﻿using AppService;
 using AppService.Entities;
 using BlazorApp.Extensions;
+using Microsoft.AspNetCore.Components.Routing;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
@@ -9,10 +10,12 @@ namespace BlazorApp;
 
 internal class EventBackgroundService(
 	EventRelay eventRelay,
+	ILogger<EventBackgroundService> logger,
 	IDbContextFactory<ApplicationDbContext> dbFactory,
 	IOptions<ConnectionStrings> connectionStrings) : BackgroundService
 {
 	private readonly EventRelay _eventRelay = eventRelay;
+	private readonly ILogger<EventBackgroundService> _logger = logger;
 	private readonly IDbContextFactory<ApplicationDbContext> _dbFactory = dbFactory;
 	private readonly ConnectionStrings _connectionStrings = connectionStrings.Value;	
 
@@ -25,13 +28,15 @@ internal class EventBackgroundService(
 			var readyMessage = await cn.DequeueAsync<EventMessage>("[dbo].[Events]", "[Game] IS NOT NULL");
 			if (readyMessage != null)
 			{
-				_eventRelay.CallReadyStateChanged(readyMessage.Game!.Value, readyMessage.ToUser, readyMessage.Topic, readyMessage.Payload);
+				_logger.LogDebug("Ready state changed: {@message}", readyMessage);
+				_eventRelay.NotifyReadyStateChanged(readyMessage.Game!.Value, readyMessage.ToUser, readyMessage.Topic, readyMessage.Payload);
 			}
 
 			var gameStateMessage = await cn.DequeueAsync<EventMessage>("[dbo].[Events]", "[GameInstanceId] IS NOT NULL");
 			if (gameStateMessage != null)
 			{
-				_eventRelay.CallGameStateChanged(gameStateMessage.GameInstanceId!.Value, gameStateMessage.ToUser, gameStateMessage.Topic, gameStateMessage.Payload);
+				_logger.LogDebug("Game state changed: {@message}", gameStateMessage);
+				_eventRelay.NotifyGameStateChanged(gameStateMessage.GameInstanceId!.Value, gameStateMessage.ToUser, gameStateMessage.Topic, gameStateMessage.Payload);
 			}
 
 			await Task.Delay(500, stoppingToken);
--- BlazorApp/EventRelay.cs
+++ BlazorApp/EventRelay.cs
@@ -1,13 +1,70 @@
 ﻿using AppService;
+using AppService.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorApp;
 
 internal delegate void ReadyStateChangedHandler(SupportedGames game, string toUser, string? topic = null, string? payload = null);
 internal delegate void GameStateChangedHandler(int gameInstanceId, string toUser, string? topic = null, string? payload = null);
 
-internal class EventRelay(ILogger<EventRelay> logger)
+internal class EventRelay(
+	IDbContextFactory<ApplicationDbContext> dbFactory,
+	ILogger<EventRelay> logger)
 {
+	private readonly IDbContextFactory<ApplicationDbContext> _dbFactory = dbFactory;
 	private readonly ILogger<EventRelay> _logger = logger;
+
+	public async Task GameStateChangedAsync(int gameInstanceId, string fromUser, string? topic = null, string? payload = null)
+	{
+		using var db = _dbFactory.CreateDbContext();
+
+		var players = await db.ActivePlayers
+			.Include(row => row.User)
+			.Where(p => p.GameInstanceId == gameInstanceId && p.User!.UserName != fromUser && !p.IsResigned)
+			.Select(row => row.User!.UserName)
+			.ToListAsync();
+
+		var messages = players.Select(player => new EventMessage()
+		{
+			GameInstanceId = gameInstanceId,
+			FromUser = fromUser,
+			ToUser = player!,
+			Topic = topic,
+			Payload = payload
+		});
+
+		db.Events.AddRange(messages);
+		await db.SaveChangesAsync();
+	}
+
+	public async Task GameStartedAsync(SupportedGames game, string fromUser, string url) =>
+		await ReadyStateChangedAsync(game, fromUser, EventMessage.GameStarted, url);
+
+	public async Task ReadyStateChangedAsync(SupportedGames game, string fromUser, string? topic = null, string? payload = null)
+	{
+		_logger.LogDebug("ReadyStateChanged invoked: {game} {fromUser}, topic = {topic}, payload = {payload}",
+			game, fromUser, topic, payload);
+
+		using var db = _dbFactory.CreateDbContext();
+
+		var players = await db.ReadyPlayers
+			.Include(row => row.User)
+			.Where(p => p.Game == game && p.User!.UserName != fromUser)
+			.Select(row => row.User!.UserName)
+			.ToListAsync();
+
+		var messages = players.Select(player => new EventMessage()
+		{
+			Game = game,
+			FromUser = fromUser,
+			ToUser = player!,
+			Topic = topic,
+			Payload = payload
+		});
+
+		db.Events.AddRange(messages);
+		await db.SaveChangesAsync();
+	}
 
 	/// <summary>
 	/// player added or removed from ready state
@@ -18,15 +75,19 @@ internal class EventRelay(ILogger<EventRelay> logger)
 	/// </summary>
 	public event GameStateChangedHandler? GameStateChanged;
 
-	public void CallReadyStateChanged(SupportedGames game, string toUser, string? topic = null, string? payload = null)
-	{
-		_logger.LogDebug("Ready state changed: {game}, {toUser}, {topic}, {payload}", game, toUser, topic, payload);
+	/// <summary>
+	/// called by background service when event message is dequeued
+	/// </summary>
+	public void NotifyReadyStateChanged(SupportedGames game, string toUser, string? topic = null, string? payload = null)
+	{		
 		ReadyStateChanged?.Invoke(game, toUser, topic, payload);
 	}
-	
-	public void CallGameStateChanged(int gameInstanceId, string toUser, string? topic = null, string? payload = null)
-	{
-		_logger.LogDebug("Game state changed: {gameInstanceId}, {toUser}, {topic}, {payload}", gameInstanceId, toUser, topic, payload);
+
+	/// <summary>
+	/// called by background service when event message is dequeued
+	/// </summary>
+	public void NotifyGameStateChanged(int gameInstanceId, string toUser, string? topic = null, string? payload = null)
+	{		
 		GameStateChanged?.Invoke(gameInstanceId, toUser, topic, payload);
 	}		
 }
