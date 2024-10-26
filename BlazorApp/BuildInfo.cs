047d625d15d4fe076c72ad503cde4f52e405feb8
Adam O'Neil <adamosoftware@gmail.com>
2024-10-26 14:17:05 -0400

    removed comment

--- BlazorApp/BuildInfo.cs
+++ BlazorApp/BuildInfo.cs
@@ -2,8 +2,6 @@
 
 namespace BlazorApp;
 
-// once more with feeling
-
 [BuildInformation(AddGitCommitId = true, AddLocalBuildTime = true, FakeIfDebug = false, FakeIfRelease = false, AddGitBranch = true)]
 partial class BuildInfo
 {
