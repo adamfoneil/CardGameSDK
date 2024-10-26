c403326713c10f62f76753a50759554176879447
Adam O'Neil <adamosoftware@gmail.com>
2024-10-26 13:35:47 -0400

    logging in github stuff

--- BlazorApp/Extensions/CacheHelper.cs
+++ BlazorApp/Extensions/CacheHelper.cs
@@ -3,18 +3,16 @@ using System.Text.Json;
 
 namespace BlazorApp.Extensions;
 
-// another dummy commit
-
 internal static class CacheHelper
 {
-	public static async Task<T> GetOrAddAsync<T>(
+	public static async Task<(T Result, bool IsLive)> GetOrAddAsync<T>(
 		this IDistributedCache cache, string key, Func<Task<T>> factory, 
 		TimeSpan expiresAfter)
 	{
 		var bytes = await cache.GetAsync(key);
 		if (bytes != null)
 		{
-			return JsonSerializer.Deserialize<T>(bytes) ?? throw new InvalidOperationException("Could not deserialize cached value.");
+			return (JsonSerializer.Deserialize<T>(bytes) ?? throw new InvalidOperationException("Could not deserialize cached value."), false);
 		}
 		
 		T value = await factory();
@@ -23,6 +21,6 @@ internal static class CacheHelper
 			AbsoluteExpirationRelativeToNow = expiresAfter
 		});
 		
-		return value;
+		return (value, true);
 	}
 }
--- BlazorApp/GitHub/GitHubClient.cs
+++ BlazorApp/GitHub/GitHubClient.cs
@@ -20,18 +20,21 @@ internal class GitHubClient
 {
     private readonly HttpClient _httpClient;
     private readonly GitHubClientOptions _options;
+	private readonly ILogger<GitHubClient> _logger;
 	private readonly IDistributedCache _cache;
 
 	public GitHubClient(
+        ILogger<GitHubClient> logger,
         IDistributedCache cache,
-        IHttpClientFactory httpClientFactory, IOptions<GitHubClientOptions> options)
+        IHttpClientFactory httpClientFactory, 
+        IOptions<GitHubClientOptions> options)
     {
         _options = options.Value;
-
         _httpClient = httpClientFactory.CreateClient();
         _httpClient.BaseAddress = new Uri("https://api.github.com/");
         _httpClient.DefaultRequestHeaders.Add("User-Agent", _options.UserAgent);
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _options.PersonalAccessToken);
+		_logger = logger;
 		_cache = cache;
 	}
 
@@ -39,21 +42,27 @@ internal class GitHubClient
 
 	public async Task<string> GetLatestCommitIdAsync()
     {
-        return await _cache.GetOrAddAsync("latest-commit-id", async () =>
+        (string result, bool isLive) = await _cache.GetOrAddAsync("latest-commit-id", async () =>
 		{
 			var response = await _httpClient.GetStringAsync($"repos/{_options.RepositoryOwner}/{_options.RepositoryName}/commits/{_options.Branch}?per_page=1");
 			var json = JsonDocument.Parse(response);
 			return json.RootElement.GetProperty("sha").GetString() ?? throw new InvalidOperationException("Could not get commit id.");
 		}, TimeSpan.FromMinutes(CacheMinutes));
+
+        _logger.LogDebug("GetLatestCommitIdAsync: {result} (live: {isLive})", result, isLive);
+
+        return result;
 	}
 
     public async Task<(int Count, string CompareUrl)> GetCommitsBehindAsync(string buildCommitId)
 	{
-        var latestCommiId = await GetLatestCommitIdAsync();
+        var latestCommiId = await GetLatestCommitIdAsync();        
 
 		var url = $"repos/{_options.RepositoryOwner}/{_options.RepositoryName}/compare/{buildCommitId}...{latestCommiId}";
 
-		return await _cache.GetOrAddAsync("commits-behind", async () =>
+		if (buildCommitId == latestCommiId) return (0, url);
+
+		((int count, string compareUrl), bool isLive) = await _cache.GetOrAddAsync("commits-behind", async () =>
         {
             try
             {                
@@ -71,5 +80,9 @@ internal class GitHubClient
                 throw;
             }
         }, TimeSpan.FromMinutes(CacheMinutes));
+
+        _logger.LogDebug("GetCommitsBehindAsync: {count} (live: {isLive})", count, isLive);
+
+        return (count, compareUrl);
 	}
 }
