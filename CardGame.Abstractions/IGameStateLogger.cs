1a770f2e4ff5af600869c8e50a46b73704c4ca6f
Adam O'Neil <adamosoftware@gmail.com>
2024-10-27 10:04:29 -0400

    don't need this

--- CardGame.Abstractions/IGameStateLogger.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace CardGame.Abstractions;
-
-public interface IGameStateLogger
-{
-	Task LogAsync(int gameInstanceId, string message, [CallerMemberName] string? methodName = null);
-}
