87e035e6366f35806820d645045072e06671d096
Adam O'Neil <adamosoftware@gmail.com>
2024-11-03 17:44:07 -0500

    inching forward on Scom

--- Games.Scom/ScomGameState.cs
+++ Games.Scom/ScomGameState.cs
@@ -1,6 +1,52 @@
-﻿namespace Games.Scom;
+﻿using CardGame.Abstractions;
 
-public class ScomGameState
+namespace Games.Scom;
+
+public class Piece
 {
+	public string Name { get; set; } = default!;
+	/// <summary>
+	/// how far can I move per turn?
+	/// </summary>
+	public int Range { get; set; }
+	/// <summary>
+	/// subtract from enemy attack roll
+	/// </summary>
+	public int Armor { get; set; }
+	/// <summary>
+	/// add to attack roll
+	/// </summary>
+	public int Attack { get; set; }	
+}
 
+public class ScomGameState : GameState<Piece>
+{
+	/// <summary>
+	/// player name + piece name -> location
+	/// </summary>
+	public Dictionary<(string, string), (int X, int Y)> Locations { get; set; } = [];
+
+	public override bool IsRoundFinished => throw new NotImplementedException();
+
+	public override Dictionary<string, int> Score => throw new NotImplementedException();
+
+	public override void AutoPlay()
+	{
+		throw new NotImplementedException();
+	}
+
+	public override (bool IsValid, string? Message) ValidatePlay(string playerName, Piece card)
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override int NextPlayerIndex(int currentIndex)
+	{
+		throw new NotImplementedException();
+	}
+
+	protected override void OnPlayCard(string playerName, Piece card)
+	{
+		throw new NotImplementedException();
+	}
 }
