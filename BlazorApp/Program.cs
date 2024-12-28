1c5f45448d660b3d643ce5226e102b4932f16034
Adam O'Neil <adamosoftware@gmail.com>
2024-12-28 07:52:34 -0500

    remove Scom, .NET9 upgrade

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -52,7 +52,7 @@ builder.Services.AddSingleton<GitHubClient>();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.Configure<ConnectionStrings>(builder.Configuration.GetSection("ConnectionStrings"));
-builder.Services.AddDbContextFactory<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options => options.UseSqlServer(connectionString).EnableSensitiveDataLogging());
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
